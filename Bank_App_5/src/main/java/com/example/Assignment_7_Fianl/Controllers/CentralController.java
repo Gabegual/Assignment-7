e1b6a8c68fe0d48a44d0c7860a13d2dc952a679b
Gabriel Gualteros <Roblox4@gmail.com>
2021-05-21 23:13:50 -0400

    added jwt still not working no communication/ maybe outdated pom depen?

--- Bank_App_5/src/main/java/com/example/Assignment_7_Fianl/Controllers/CentralController.java
+++ Bank_App_5/src/main/java/com/example/Assignment_7_Fianl/Controllers/CentralController.java
@@ -6,22 +6,41 @@ import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Assignment_7_Fianl.*;
+import com.example.Assignment_7_Fianl.Security_Config.MyUserDetailsService;
 import com.example.Assignment_7_Fianl.exceptions.*;
 import com.example.Assignment_7_Fianl.models.*;
 
+import jwtUtil.JwtUtil;
+
 @RestController
 public class CentralController {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+	@Autowired
+	private JwtUtil jwtTokenUtil;
 
 	Logger logger = LoggerFactory.getLogger(CentralController.class);
 
@@ -39,6 +58,22 @@ public class CentralController {
 	@GetMapping("/admin")
 	public String admin() {
 		return ("<h1> Welcome Admin Sir OwO </h1>");
+	}
+
+	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+	public ResponseEntity<?> createauthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
+			throws Exception {
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+					authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+		} catch (BadCredentialsException e) {
+			throw new Exception("Incorrect username or password", e);
+		}
+
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+		final String jwt = jwtTokenUtil.generateToken(userDetails);
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
+
 	}
 
 	@PostMapping(value = "/admin/AccountHolders")
--- Bank_App_5/src/main/java/com/example/Assignment_7_Fianl/Security_Config/Spring_Securtiy_Config.java
+++ Bank_App_5/src/main/java/com/example/Assignment_7_Fianl/Security_Config/Spring_Securtiy_Config.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -31,17 +32,27 @@ public class Spring_Securtiy_Config extends WebSecurityConfigurerAdapter {
 //		.roles("Admin");
 		
 		auth.userDetailsService(userDetailsService);
+		
 
+	}
+	
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 
 	@Bean
 	public PasswordEncoder getPasswordEncodder() {
 		return NoOpPasswordEncoder.getInstance();
-
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests()
+		.antMatchers("/authenticate").permitAll()
+		.anyRequest().authenticated();
+		
 		http.authorizeRequests()
 			.antMatchers("/").permitAll()
 			.antMatchers("/user/**").hasAnyRole("Admin", "User")
